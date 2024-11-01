a4b8d1079fa28d337bfa0cbcd0d1d8c812af30f9
maya-nachiappan <maya.n102304@gmail.com>
2024-10-31 23:20:22 -0700

    linked the files

---
+++ arraySum.hpp
@@ -0,0 +1,13 @@
+//
+//  arraySum.hpp
+//  
+//
+//  Created by maya nachiappan on 10/31/24.
+//
+
+#ifndef arraySum_hpp
+#define arraySum_hpp
+
+#include <stdio.h>
+void arraySum();
+#endif /* arraySum_hpp */
---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  dynamic array printer
+//
+//  Created by maya nachiappan on 10/14/24.
+//
+
+#include <iostream>
+#include "arraySum.hpp"
+using namespace std;
+
+int main()
+{
+    arraySum();
+    return 0;
+}
+
